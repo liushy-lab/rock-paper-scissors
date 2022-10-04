0806146b6c8bf079bc339f033c67e18745f8f9bb
liushy-lab <liudmila.shynkevich@gmail.com>
2022-10-04 16:26:35 +0300

    Add some notes to the html file

---
+++ script.js
@@ -0,0 +1,18 @@
+// set variables for the game, playRound = 0 or 1 or it should't be set?
+let playerSelection = 0;
+let computerSelection = 0;
+
+
+function getComputerChoice () {
+    // set the choices for the comp., those should be randomized during the game
+    let choice = ['rock' 'paper', 'scissors']
+    return choice(Math.random());
+}
+
+function playRound(playerSelection, computerSelection) {
+    // compare player's and computer's inputs and choose the winner
+
+    function playRound(){
+        // set 5 rounds max for the game, should it be with 
+        for (let i = 0; i < 5; i++)
+    }
